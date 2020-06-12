f78b2c541472c8a0a477bd23a6aa9aa1713aebf1
QingQing <183129h@mymail.nyp.edu.sg>
2020-06-11 19:48:43 -0700

    Initial Commit

---
+++ SDDP-iFit-Team3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SDDP-iFit-Team3
+//
+//  Created by M09-1 on 6/11/20.
+//  Copyright © 2020 SDDP_Team3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
