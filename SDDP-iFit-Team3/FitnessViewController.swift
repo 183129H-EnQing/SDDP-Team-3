c83e5df9309f88aed12f969f166aee67e516d508
A3uG <44191224+A3uG@users.noreply.github.com>
2020-06-22 01:29:21 -0700

    minor adds
    game ref not properly

--- SDDP-iFit-Team3/FitnessViewController.swift
+++ SDDP-iFit-Team3/FitnessViewController.swift
@@ -9,15 +9,7 @@
 import Foundation
 import UIKit
 
-class FitnessVIewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-    
+class FitnessVIewController: UIViewController {
+
     
 }
