8f47c6e2997629499f6e3abb9007d4dc1895fd07
A3uG <44191224+A3uG@users.noreply.github.com>
2020-06-21 22:25:31 -0700

    new Fitness files
    Exercise model.. FitnessViewController

---
+++ SDDP-iFit-Team3/Exercise.swift
@@ -0,0 +1,22 @@
+//
+//  Exercise.swift
+//  SDDP-iFit-Team3
+//
+//  Created by 182452K  on 6/21/20.
+//  Copyright © 2020 SDDP_Team3. All rights reserved.
+//
+
+import UIKit
+
+class Exercise: NSObject {
+
+    var exName: String
+    var exDesc : String
+    var exImage: String
+    
+    init(exName: String, exDesc: String, exImage: String){
+        self.exName = exName
+        self.exDesc = exDesc
+        self.exImage = exImage
+    }
+}
---
+++ SDDP-iFit-Team3/FitnessViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FitnessViewController.swift
+//  SDDP-iFit-Team3
+//
+//  Created by 182452K  on 6/21/20.
+//  Copyright © 2020 SDDP_Team3. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FitnessVIewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
