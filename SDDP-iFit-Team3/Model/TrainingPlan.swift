33b51594da6a3fabfda45f183f51fa081ebebd13
A3uG <44191224+A3uG@users.noreply.github.com>
2020-06-22 21:55:16 -0700

    new fitness files and UI
    added trainingplan model, fitness page images

---
+++ SDDP-iFit-Team3/Model/TrainingPlan.swift
@@ -0,0 +1,24 @@
+//
+//  TrainingPlan.swift
+//  SDDP-iFit-Team3
+//
+//  Created by 182452K  on 6/22/20.
+//  Copyright © 2020 SDDP_Team3. All rights reserved.
+//
+
+import Foundation
+
+class TrainingPlan: NSObject {
+    
+    var tpName: String
+    var tpDesc: String
+    var tpReps : Int
+    var tpExercises: [String]
+    
+    init(tpName: String, tpDesc: String, tpReps: Int, tpExercises: [String]){
+        self.tpName = tpName
+        self.tpDesc = tpDesc
+        self.tpReps = tpReps
+        self.tpExercises = tpExercises
+    }
+}
