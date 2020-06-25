577c6d7e6e8d778e55d5cd3794e7fa6e2a29c3d8
A3uG <44191224+A3uG@users.noreply.github.com>
2020-06-24 21:41:22 -0700

    training plan -- add exercise
    new files and list linkage done. check is not shown

---
+++ SDDP-iFit-Team3/Controller/TrainingPlanAddExerciseViewController.swift
@@ -0,0 +1,47 @@
+//
+//  TrainingPlanAddExerciseViewController.swift
+//  SDDP-iFit-Team3
+//
+//  Created by 182452K  on 6/24/20.
+//  Copyright © 2020 SDDP_Team3. All rights reserved.
+//
+
+import UIKit
+
+class TrainingPlanAddExerciseViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var exerciseList : [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        exerciseList = ["Plank on forearms", "ELevated crunches", "Push-up", "Sit-up"]
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return exerciseList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TPExerciseCell", for: indexPath)
+        let e = exerciseList[indexPath.row]
+        cell.textLabel?.text = e
+        
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
