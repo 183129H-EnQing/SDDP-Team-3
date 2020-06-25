050584581fb516b35f010d67889141e01e89dd6f
DINESH <180116h@mymail.nyp.edu.sg>
2020-06-25 08:19:07 -0700

    moved addpost to folder

--- SDDP-iFit-Team3/Controller/AddPostViewController.swift
+++ SDDP-iFit-Team3/Controller/AddPostViewController.swift
@@ -12,7 +12,8 @@ class AddPostViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
         // Do any additional setup after loading the view.
     }
     
