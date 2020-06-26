354605743cf6c35f1c411d598daaaf20e9980763
Guan Hong <182381J@mymail.nyp.edu.sg>
2020-06-26 06:24:22 -0700

    added dashboard basic design

---
+++ SDDP-iFit-Team3/Controller/CardDesign.swift
@@ -0,0 +1,28 @@
+//
+//  CardDesign.swift
+//  SDDP-iFit-Team3
+//
+//  Created by 182381J  on 6/26/20.
+//  Copyright © 2020 SDDP_Team3. All rights reserved.
+//
+
+import UIKit
+
+class CardDesign: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    override func layoutSubviews() {
+            layer.cornerRadius = 20.0
+             layer.shadowColor = UIColor.gray.cgColor
+             layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+             layer.shadowRadius = 12.0
+             layer.shadowOpacity = 0.7
+    }
+     
+}
