325f4d06733fb198870de915985abeb7adbd1c2d
Lam En Qing <183129h@mymail.nyp.edu.sg>
2020-06-26 22:45:28 -0700

    Created helper class for our own functions: checks if string can be parsed into integer, and making an alert

---
+++ SDDP-iFit-Team3/Helper/Team3Helper.swift
@@ -0,0 +1,36 @@
+//
+//  Team3Helper.swift
+//  SDDP-iFit-Team3
+//
+//  Created by 183129H  on 6/26/20.
+//  Copyright © 2020 SDDP_Team3. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Team3Helper {
+    // since this function is needed in more than 1 class, made a helper class to put re-used functions
+    static func makeAlert(_ message:String) -> UIAlertController {
+       let alert = UIAlertController(
+           title: message,
+           message: "",
+           preferredStyle: UIAlertController.Style.alert)
+       
+       alert.addAction(
+           UIAlertAction(
+               title: "OK",
+               style: UIAlertAction.Style.default,
+               handler: nil
+           )
+       )
+       
+       return alert
+    }
+    
+    static func ifInputIsInt(someInput: String) -> Bool {
+        guard let parsedNumber = Int(someInput) else {return false}
+        
+        return true
+    }
+}
