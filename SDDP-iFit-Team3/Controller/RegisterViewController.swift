decffd6fed839760485126ad75a8e9bb09d9213a
Lam En Qing <183129h@mymail.nyp.edu.sg>
2020-06-30 23:56:11 -0700

    Added register screen fields

--- SDDP-iFit-Team3/Controller/RegisterViewController.swift
+++ SDDP-iFit-Team3/Controller/RegisterViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextFIeld: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
