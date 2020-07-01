b7e8df12a4fbabaa26119a286954bfcd153404e3
Lam En Qing <183129h@mymail.nyp.edu.sg>
2020-06-30 23:39:31 -0700

    Created login and register view controllers, added navigation controller for the welcome screen

---
+++ SDDP-iFit-Team3/Controller/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  SDDP-iFit-Team3
+//
+//  Created by 183129H  on 6/30/20.
+//  Copyright © 2020 SDDP_Team3. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
