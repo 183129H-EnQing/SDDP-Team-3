bbedb36b2bf4ad1b768b0e16c5ad3e99a68b6ba2
A3uG <44191224+A3uG@users.noreply.github.com>
2020-07-02 06:38:01 -0700

    improved game

--- SDDP-iFit-Team3/Model/GameScene.swift
+++ SDDP-iFit-Team3/Model/GameScene.swift
@@ -10,17 +10,68 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    let player = SKSpriteNode(imageNamed: "survivor_handgun")
+    
+    let bulletSound = SKAction.playSoundFileNamed("bulletSound_3", waitForCompletion: false)
+    
+    var gameArea: CGRect
+    
+    override init(size: CGSize) {
+        
+        let maxAspectRatio: CGFloat = 16.0/9.0
+        let playableWidth = size.height/maxAspectRatio
+        let margin = (size.width - playableWidth)/2
+        gameArea = CGRect(x: margin, y: 0, width: playableWidth, height: size.height)
+        
+        super.init(size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func didMove(to view: SKView) {
         
         let background = SKSpriteNode(imageNamed: "pull_string")
         background.size = self.size
-        background.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        background.position = CGPoint(x: self.size.width/2, y: self.size.height * 0.2)
         background.zPosition = 0
         self.addChild(background)
         
-        let player = SKSpriteNode(imageNamed: "pull_string")
+        
         player.setScale(1)
-        player.position = CGPoint()
+        player.position = CGPoint(x: self.size.width/2, y: self.size.height * 0.2)
+        player.zPosition = 2
+        self.addChild(player)
+    }
+    
+    func fireBullet(){
+        
+        let bullet = SKSpriteNode(imageNamed: "bullet")
+        bullet.setScale(1)
+        bullet.position = player.position 
+        player.zPosition = 1
+        self.addChild(bullet)
+        
+        let moveBullet = SKAction.moveTo(y: self.size.height + bullet.size.height, duration: 1)
+        let deleteBullet = SKAction.removeFromParent()
+        let bulletSequence = SKAction.sequence([bulletSound, moveBullet, deleteBullet])
+        bullet.run(bulletSequence)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+        fireBullet()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?){
+        for touch: AnyObject in touches {
+            let pointOfTouch = touch.location(in: self)
+            let previousPointOfTouch = touch.previousLocation(in: self)
+            
+            let amountDragged = pointOfTouch.x - previousPointOfTouch.x
+            
+            player.position.x += amountDragged
+        }
     }
 }
 
