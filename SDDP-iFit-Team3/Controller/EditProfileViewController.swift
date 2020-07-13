96a94615b5f0e0a7abad81e9f99bbef6ff094365
Lam En Qing <183129h@mymail.nyp.edu.sg>
2020-07-13 12:53:07 +0800

    Edit Profile can change user's avatar now. Added logout button, but logout functionality not working

--- SDDP-iFit-Team3/Controller/EditProfileViewController.swift
+++ SDDP-iFit-Team3/Controller/EditProfileViewController.swift
@@ -53,8 +53,9 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
             StorageManager.uploadUserProfile(userId: user.uid, image: avatarImgView.image!)
         }
     }
-    /*
-    // MARK: - Navigation
+    
+     /*
+     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
