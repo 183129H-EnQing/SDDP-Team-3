81f4011e3e3f954088f51a01c95afb9a36e8a832
A3uG <44191224+A3uG@users.noreply.github.com>
2020-07-21 05:48:13 -0700

    show planets accordingly
    discover button works!

--- SDDP-iFit-Team3/Model/GamePlanet.swift
+++ SDDP-iFit-Team3/Model/GamePlanet.swift
@@ -15,6 +15,7 @@ class GamePlanet: SKScene {
     var playerTroops = 10
     
     var planetsList = ["earth", "fireball", "redcrater", "desert", "cat", "eagle"]
+    var planetsTextList = ["EARTH", "FIREBALL", "RED CRATER", "DESERT", "CAT'S PLAY", "EAGLE'S NEST"]
     
     override func didMove(to view: SKView) {
         let bg = SKSpriteNode(imageNamed: "space")
@@ -84,90 +85,92 @@ class GamePlanet: SKScene {
         //
         
         //
-        let earth = SKSpriteNode(imageNamed: "earth")
-        earth.size = CGSize(width: self.size.width * 0.2, height: self.size.height * 0.15)
-        earth.position = CGPoint(x: self.size.width * 0.35, y: self.size.height * 0.7)
-        earth.zPosition = 1
-        self.addChild(earth)
+//        let earth = SKSpriteNode(imageNamed: "earth")
+//        earth.size = CGSize(width: self.size.width * 0.2, height: self.size.height * 0.15)
+//        earth.position = CGPoint(x: self.size.width * 0.35, y: self.size.height * 0.7)
+//        earth.zPosition = 1
+//        self.addChild(earth)
+//
+//        let fireball = SKSpriteNode(imageNamed: "fireball")
+//        fireball.size = CGSize(width: self.size.width * 0.2, height: self.size.height * 0.15)
+//        fireball.position = CGPoint(x: self.size.width * 0.65, y: self.size.height * 0.7)
+//        fireball.zPosition = 1
+//        self.addChild(fireball)
         
-        let fireball = SKSpriteNode(imageNamed: "fireball")
-        fireball.size = CGSize(width: self.size.width * 0.2, height: self.size.height * 0.15)
-        fireball.position = CGPoint(x: self.size.width * 0.65, y: self.size.height * 0.7)
-        fireball.zPosition = 1
-        self.addChild(fireball)
-        
-        let redcrater = SKSpriteNode(imageNamed: "redcrater")
-        redcrater.size = CGSize(width: self.size.width * 0.2, height: self.size.height * 0.15)
-        redcrater.position = CGPoint(x: self.size.width * 0.35, y: self.size.height * 0.5)
-        redcrater.zPosition = 1
-        self.addChild(redcrater)
-        
-        let desert = SKSpriteNode(imageNamed: "desert")
-        desert.size = CGSize(width: self.size.width * 0.2, height: self.size.height * 0.15)
-        desert.position = CGPoint(x: self.size.width * 0.65, y: self.size.height * 0.5)
-        desert.zPosition = 1
-        self.addChild(desert)
-        
-        let cat = SKSpriteNode(imageNamed: "cat")
-        cat.size = CGSize(width: self.size.width * 0.2, height: self.size.height * 0.15)
-        cat.position = CGPoint(x: self.size.width * 0.35, y: self.size.height * 0.3)
-        cat.zPosition = 1
-        self.addChild(cat)
-        
-        let egg = SKSpriteNode(imageNamed: "egg")
-        egg.size = CGSize(width: self.size.width * 0.2, height: self.size.height * 0.15)
-        egg.position = CGPoint(x: self.size.width * 0.65, y: self.size.height * 0.3)
-        egg.zPosition = 1
-        self.addChild(egg)
-        
+//        let redcrater = SKSpriteNode(imageNamed: "redcrater")
+//        redcrater.size = CGSize(width: self.size.width * 0.2, height: self.size.height * 0.15)
+//        redcrater.position = CGPoint(x: self.size.width * 0.35, y: self.size.height * 0.5)
+//        redcrater.zPosition = 1
+//        self.addChild(redcrater)
+//
+//        let desert = SKSpriteNode(imageNamed: "desert")
+//        desert.size = CGSize(width: self.size.width * 0.2, height: self.size.height * 0.15)
+//        desert.position = CGPoint(x: self.size.width * 0.65, y: self.size.height * 0.5)
+//        desert.zPosition = 1
+//        self.addChild(desert)
+//
+//        let cat = SKSpriteNode(imageNamed: "cat")
+//        cat.size = CGSize(width: self.size.width * 0.2, height: self.size.height * 0.15)
+//        cat.position = CGPoint(x: self.size.width * 0.35, y: self.size.height * 0.3)
+//        cat.zPosition = 1
+//        self.addChild(cat)
+//
+//        let egg = SKSpriteNode(imageNamed: "egg")
+//        egg.size = CGSize(width: self.size.width * 0.2, height: self.size.height * 0.15)
+//        egg.position = CGPoint(x: self.size.width * 0.65, y: self.size.height * 0.3)
+//        egg.zPosition = 1
+//        self.addChild(egg)
+//
         //
-        let earthText = SKLabelNode(fontNamed: "The Bold Font")
-        earthText.text = "Earth"
-        earthText.fontSize = 45
-        earthText.fontColor = SKColor.white
-        earthText.position = CGPoint(x: self.size.width * 0.35, y: self.size.height * 0.6)
-        earthText.zPosition = 1
-        self.addChild(earthText)
+//        let earthText = SKLabelNode(fontNamed: "The Bold Font")
+//        earthText.text = "Earth"
+//        earthText.fontSize = 45
+//        earthText.fontColor = SKColor.white
+//        earthText.position = CGPoint(x: self.size.width * 0.35, y: self.size.height * 0.6)
+//        earthText.zPosition = 1
+//        self.addChild(earthText)
+//
+//        let fireballText = SKLabelNode(fontNamed: "The Bold Font")
+//        fireballText.text = "Fireball"
+//        fireballText.fontSize = 45
+//        fireballText.fontColor = SKColor.white
+//        fireballText.position = CGPoint(x: self.size.width * 0.65, y: self.size.height * 0.6)
+//        fireballText.zPosition = 1
+//        self.addChild(fireballText)
+//
+//        let redcraterText = SKLabelNode(fontNamed: "The Bold Font")
+//        redcraterText.text = "Red Crater"
+//        redcraterText.fontSize = 45
+//        redcraterText.fontColor = SKColor.white
+//        redcraterText.position = CGPoint(x: self.size.width * 0.35, y: self.size.height * 0.4)
+//        redcraterText.zPosition = 1
+//        self.addChild(redcraterText)
+//
+//        let desertText = SKLabelNode(fontNamed: "The Bold Font")
+//        desertText.text = "Desert"
+//        desertText.fontSize = 45
+//        desertText.fontColor = SKColor.white
+//        desertText.position = CGPoint(x: self.size.width * 0.65, y: self.size.height * 0.4)
+//        desertText.zPosition = 1
+//        self.addChild(desertText)
+//
+//        let catText = SKLabelNode(fontNamed: "The Bold Font")
+//        catText.text = "Cat's Play"
+//        catText.fontSize = 45
+//        catText.fontColor = SKColor.white
+//        catText.position = CGPoint(x: self.size.width * 0.35, y: self.size.height * 0.2)
+//        catText.zPosition = 1
+//        self.addChild(catText)
+//
+//        let eggText = SKLabelNode(fontNamed: "The Bold Font")
+//        eggText.text = "Eagle's Nest"
+//        eggText.fontSize = 45
+//        eggText.fontColor = SKColor.white
+//        eggText.position = CGPoint(x: self.size.width * 0.65, y: self.size.height * 0.2)
+//        eggText.zPosition = 1
+//        self.addChild(eggText)
         
-        let fireballText = SKLabelNode(fontNamed: "The Bold Font")
-        fireballText.text = "Fireball"
-        fireballText.fontSize = 45
-        fireballText.fontColor = SKColor.white
-        fireballText.position = CGPoint(x: self.size.width * 0.65, y: self.size.height * 0.6)
-        fireballText.zPosition = 1
-        self.addChild(fireballText)
-        
-        let redcraterText = SKLabelNode(fontNamed: "The Bold Font")
-        redcraterText.text = "Red Crater"
-        redcraterText.fontSize = 45
-        redcraterText.fontColor = SKColor.white
-        redcraterText.position = CGPoint(x: self.size.width * 0.35, y: self.size.height * 0.4)
-        redcraterText.zPosition = 1
-        self.addChild(redcraterText)
-        
-        let desertText = SKLabelNode(fontNamed: "The Bold Font")
-        desertText.text = "Desert"
-        desertText.fontSize = 45
-        desertText.fontColor = SKColor.white
-        desertText.position = CGPoint(x: self.size.width * 0.65, y: self.size.height * 0.4)
-        desertText.zPosition = 1
-        self.addChild(desertText)
-        
-        let catText = SKLabelNode(fontNamed: "The Bold Font")
-        catText.text = "Cat's Play"
-        catText.fontSize = 45
-        catText.fontColor = SKColor.white
-        catText.position = CGPoint(x: self.size.width * 0.35, y: self.size.height * 0.2)
-        catText.zPosition = 1
-        self.addChild(catText)
-        
-        let eggText = SKLabelNode(fontNamed: "The Bold Font")
-        eggText.text = "Eagle's Nest"
-        eggText.fontSize = 45
-        eggText.fontColor = SKColor.white
-        eggText.position = CGPoint(x: self.size.width * 0.65, y: self.size.height * 0.2)
-        eggText.zPosition = 1
-        self.addChild(eggText)
+        loadPlanet()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -212,7 +215,8 @@ class GamePlanet: SKScene {
             sufficientCost = true
             //not working yet
             playerFitness -= planetCost
-            print("SCAMMMM ", playerFitness)
+            playerPlanets += 1
+//            print("SCAMMMM ", playerFitness)
             
             let sceneChange = GameDiscover(size: self.size)
             sceneChange.scaleMode = self.scaleMode
@@ -228,14 +232,127 @@ class GamePlanet: SKScene {
     }
     
     func loadPlanet() {
+        //rmb call functions
+        //load planets, else show qns mark images
         
-        //rmb call function
-        var sum = 0
-        for i in planetsList {
-            sum += 1
-            if sum < playerPlanets {
-                //load planets, else show qns mark image
+        var planetImage = [String]()
+        var planetText = [String]()
+        
+        for i in 0..<planetsList.count{
+            
+//            print("OII", i)
+            if i <= playerPlanets {
+//                print("HEYYYY ", playerPlanets)
+                switch i {
+                case 0: planetImage.append(planetsList[0])
+                        planetText.append(planetsTextList[0])
+                case 1: planetImage.append(planetsList[1])
+                        planetText.append(planetsTextList[1])
+                case 2: planetImage.append(planetsList[2])
+                        planetText.append(planetsTextList[2])
+                case 3: planetImage.append(planetsList[3])
+                        planetText.append(planetsTextList[3])
+                case 4: planetImage.append(planetsList[4])
+                        planetText.append(planetsTextList[4])
+                case 5: planetImage.append(planetsList[5])
+                        planetText.append(planetsTextList[5])
+                default: planetImage.append("mystery")
+                        planetText.append("MYSTERY")
+                }
             }
+            else {
+                planetImage.append("mystery")
+                planetText.append("MYSTERY")
+            }
+        }
+        
+        for i in 0..<planetImage.count {
+            
+            let earth = SKSpriteNode(imageNamed: planetImage[0])
+            earth.size = CGSize(width: self.size.width * 0.2, height: self.size.height * 0.15)
+            earth.position = CGPoint(x: self.size.width * 0.35, y: self.size.height * 0.7)
+            earth.zPosition = 1
+            self.addChild(earth)
+            
+            let fireball = SKSpriteNode(imageNamed: planetImage[1])
+            fireball.size = CGSize(width: self.size.width * 0.2, height: self.size.height * 0.15)
+            fireball.position = CGPoint(x: self.size.width * 0.65, y: self.size.height * 0.7)
+            fireball.zPosition = 1
+            self.addChild(fireball)
+            
+            let redcrater = SKSpriteNode(imageNamed: planetImage[2])
+            redcrater.size = CGSize(width: self.size.width * 0.2, height: self.size.height * 0.15)
+            redcrater.position = CGPoint(x: self.size.width * 0.35, y: self.size.height * 0.5)
+            redcrater.zPosition = 1
+            self.addChild(redcrater)
+            
+            let desert = SKSpriteNode(imageNamed: planetImage[3])
+            desert.size = CGSize(width: self.size.width * 0.2, height: self.size.height * 0.15)
+            desert.position = CGPoint(x: self.size.width * 0.65, y: self.size.height * 0.5)
+            desert.zPosition = 1
+            self.addChild(desert)
+            
+            let cat = SKSpriteNode(imageNamed: planetImage[4])
+            cat.size = CGSize(width: self.size.width * 0.2, height: self.size.height * 0.15)
+            cat.position = CGPoint(x: self.size.width * 0.35, y: self.size.height * 0.3)
+            cat.zPosition = 1
+            self.addChild(cat)
+            
+            let egg = SKSpriteNode(imageNamed: planetImage[5])
+            egg.size = CGSize(width: self.size.width * 0.2, height: self.size.height * 0.15)
+            egg.position = CGPoint(x: self.size.width * 0.65, y: self.size.height * 0.3)
+            egg.zPosition = 1
+            self.addChild(egg)
+            
+            //
+            let earthText = SKLabelNode(fontNamed: "The Bold Font")
+            earthText.text = planetText[0]
+            earthText.fontSize = 45
+            earthText.fontColor = SKColor.white
+            earthText.position = CGPoint(x: self.size.width * 0.35, y: self.size.height * 0.6)
+            earthText.zPosition = 1
+            self.addChild(earthText)
+            
+            let fireballText = SKLabelNode(fontNamed: "The Bold Font")
+            fireballText.text = planetText[1]
+            fireballText.fontSize = 45
+            fireballText.fontColor = SKColor.white
+            fireballText.position = CGPoint(x: self.size.width * 0.65, y: self.size.height * 0.6)
+            fireballText.zPosition = 1
+            self.addChild(fireballText)
+            
+            let redcraterText = SKLabelNode(fontNamed: "The Bold Font")
+            redcraterText.text = planetText[2]
+            redcraterText.fontSize = 45
+            redcraterText.fontColor = SKColor.white
+            redcraterText.position = CGPoint(x: self.size.width * 0.35, y: self.size.height * 0.4)
+            redcraterText.zPosition = 1
+            self.addChild(redcraterText)
+            
+            let desertText = SKLabelNode(fontNamed: "The Bold Font")
+            desertText.text = planetText[3]
+            desertText.fontSize = 45
+            desertText.fontColor = SKColor.white
+            desertText.position = CGPoint(x: self.size.width * 0.65, y: self.size.height * 0.4)
+            desertText.zPosition = 1
+            self.addChild(desertText)
+            
+            let catText = SKLabelNode(fontNamed: "The Bold Font")
+            catText.text = planetText[4]
+            catText.fontSize = 45
+            catText.fontColor = SKColor.white
+            catText.position = CGPoint(x: self.size.width * 0.35, y: self.size.height * 0.2)
+            catText.zPosition = 1
+            self.addChild(catText)
+            
+            let eggText = SKLabelNode(fontNamed: "The Bold Font")
+            eggText.text = planetText[5]
+            eggText.fontSize = 45
+            eggText.fontColor = SKColor.white
+            eggText.position = CGPoint(x: self.size.width * 0.65, y: self.size.height * 0.2)
+            eggText.zPosition = 1
+            self.addChild(eggText)
+            
         }
     }
 }
