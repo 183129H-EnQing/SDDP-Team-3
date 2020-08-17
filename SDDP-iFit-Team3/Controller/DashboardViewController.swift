aced9721972da5710d0d39472d386019c53dc892
Prowind <182381J@mymail.nyp.edu.sg>
2020-08-17 15:18:28 -0700

    updated dashboard design

--- SDDP-iFit-Team3/Controller/DashboardViewController.swift
+++ SDDP-iFit-Team3/Controller/DashboardViewController.swift
@@ -15,13 +15,33 @@ class DashboardViewController: UIViewController {
 
     @IBOutlet weak var profileBarButton: UIBarButtonItem!
     
+    @IBOutlet weak var CircularProgress: CircularProgressView!
     override func viewDidLoad() {
         super.viewDidLoad()
+//        let cp = CircularProgressView(frame: CGRect(x: 10.0, y: 10.0, width: 100.0, height: 100.0))
+//              cp.trackColor = UIColor.white
+//              cp.progressColor = UIColor(red: 252.0/255.0, green: 141.0/255.0, blue: 165.0/255.0, alpha: 1.0)
+//
+//              cp.tag = 101
+//              self.view.addSubview(cp)
+//              cp.center = self.view.center
+//
+//              self.perform(#selector(animateProgress), with: nil, afterDelay: 2.0)
 
+              CircularProgress.trackColor = UIColor.white
+              CircularProgress.progressColor = UIColor.purple
+        CircularProgress.setProgressWithAnimation(duration: 0.0, value: 0.3)
+        
         // Do any additional setup after loading the view.
         //Team3Helper.makeImgViewRound(profileBarButton!)
     }
     
+    @objc func animateProgress() {
+          let cP = self.view.viewWithTag(101) as! CircularProgressView
+          cP.setProgressWithAnimation(duration: 0, value: 0.7)
+          
+    }
+        
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
